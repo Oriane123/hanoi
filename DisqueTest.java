4a2d880be75c312a2aa62133dd01f645a72200e8
Oriane123 <57477871+Oriane123@users.noreply.github.com>
2020-12-14 10:58:36 +0100

    Add test and hanoi by upload
    because eclipse don't want

---
+++ DisqueTest.java
@@ -0,0 +1,94 @@
+package cfranc.hanoi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DisqueTest {
+	
+	Disque dSmall = new Disque(1);
+	Disque dMedium = new Disque(2);
+	Disque dTall = new Disque(3);
+
+	@Test
+	public void testCompareTo() {
+		Disque petit = new Disque(1);
+		Disque moyen = new Disque(2);
+		Disque grand = new Disque(3);
+		assertEquals(-1, petit.compareTo(moyen));
+		assertEquals(-1, moyen.compareTo(grand));
+		//TODO : à compléter
+        assertEquals(-1, petit.compareTo(grand));
+        assertEquals(1, grand.compareTo(petit));
+        assertEquals(1, moyen.compareTo(petit));
+        assertEquals(1, grand.compareTo(moyen));
+        assertEquals(0, petit.compareTo(petit));
+        assertEquals(0, moyen.compareTo(moyen));
+        assertEquals(0, grand.compareTo(grand));
+        
+	}
+
+	@Test
+	public void compareTo_SmallMedium_Negative(){
+		int expected = -1;
+		int actual = dSmall.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_MediumTall_Negative(){
+		int expected = -1;
+		int actual = dMedium.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_SmallTall_Negative(){
+		int expected = -1;
+		int actual = dSmall.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_TallSmall_Positive(){
+		int expected = 1;
+		int actual = dTall.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_MediumSmall_Positive(){
+		int expected = 1;
+		int actual = dMedium.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_TallMedium_Positive(){
+		int expected = 1;
+		int actual = dTall.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_TallTall_Neutre(){
+		int expected = 0;
+		int actual = dTall.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_SmallSmall_Neutre(){
+		int expected = 0;
+		int actual = dSmall.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_MediumMedium_Neutre(){
+		int expected = 0;
+		int actual = dMedium.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+	
+}
